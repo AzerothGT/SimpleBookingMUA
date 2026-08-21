ccdf54a8e17b3e34a7ab2b819eb25374de39a990
AzerothGT <azerothgt@gmail.com>
2026-08-21 19:51:06 +0700

    Generate Swagger docs at runtime via generate_always

--- backend-mua/railway/init-app.sh
+++ backend-mua/railway/init-app.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 php artisan migrate --force
 php artisan optimize:clear
-php artisan l5-swagger:generate
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
